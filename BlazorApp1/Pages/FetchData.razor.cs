eab4c4b5282c8a9bb8b1f0628b5bf003200dd985
waramdani <waramdani@RABCSDDWT2>
2023-03-30 19:51:06 +0000

    first commit

---
+++ BlazorApp1/Pages/FetchData.razor.cs
@@ -0,0 +1,18 @@
+﻿using BlazorApp1.Data;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[]? forecasts;
+
+
+        [Inject] WeatherForecastService ForecastService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
